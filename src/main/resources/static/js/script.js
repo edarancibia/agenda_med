03dba0a07432dcd692d80f2f11e06effea1f23b4
edarancibia <edarancibia11@gmail.com>
2020-10-04 16:39:44 -0300

    cambio base url

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var base_url = 'http://localhost:8080/';
+	var base_url = 'http://clinic-calendar.herokuapp.com/';
 
 	$('.input-number').on('input', function () { 
     	this.value = this.value.replace(/[^0-9]/g,'');
