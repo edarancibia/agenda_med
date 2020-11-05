27c3b0206567b85b9ae6a65a6663aa2601c3135f
edarancibia <edarancibia11@gmail.com>
2020-11-04 23:29:10 -0300

    uppercase Nombres

--- src/main/java/com/calendar/repository/UserJpaRepository.java
+++ src/main/java/com/calendar/repository/UserJpaRepository.java
@@ -19,7 +19,7 @@ public interface UserJpaRepository extends JpaRepository<User, Long> {
 	
 	@Query(value = "select \n" + 
 			"u.id,\n" + 
-			"concat(u.a_pat,' ',u.a_mat,' ',u.nombre) as nombreProfesional\n" + 
+			"UPPER(concat(u.a_pat,' ',u.a_mat,' ',u.nombre)) as nombreProfesional\n" + 
 			"from users u, profesional p\n" + 
 			"where u.perfil = 1 and u.id = p.fk_idUsuario and p.cod_centro=:idCentro and u.vigente = 1", nativeQuery = true)
 	List<Map<String, Object>> getUserProfesionals(@Param("idCentro") Long idCentro);
@@ -27,7 +27,7 @@ public interface UserJpaRepository extends JpaRepository<User, Long> {
 	public abstract User findUserByEmailAndVigente(String email, int vigente);
 	
 	//LISTA DE USUARIOS PARA PANEL DE ADMINISTRACION
-	@Query(value="select u.id, concat(u.a_pat,' ',u.a_mat,' ',u.nombre) as nombre, \n" + 
+	@Query(value="select u.id, UPPER(concat(u.a_pat,' ',u.a_mat,' ',u.nombre)) as nombre, \n" + 
 			"if(u.perfil = 1,'Atención Profesional','Agenda') as perfil \n" + 
 			"from users u, usuario_centro uc \n" + 
 			"where u.id = uc.idUsuario and uc.idCentro = :idCentro and u.vigente = 1", nativeQuery = true)
